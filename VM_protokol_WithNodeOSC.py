d5a57b563c500f89d35ecba606ed9e273c32531f
LionGartGithubPage <77560129+LionGartGithubPage@users.noreply.github.com>
2021-07-27 12:15:24 +0700

    Add files via upload

---
+++ VM_protokol_WithNodeOSC.py
@@ -0,0 +1,189 @@
+OSC - Receiving:['/VMC/Ext/T', 1084.21826171875]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/OK', 1]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Tra/Pos', 'RightFoot', 0.038644250482320786, 0.009866148233413696, -0.10579195618629456, 0.23125259578227997, -0.8771951198577881, -0.4171319007873535, 0.05524668097496033]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Tra/Pos', 'LeftFoot', -0.4275943636894226, 0.10674741864204407, 0.06369905918836594, -0.5466647744178772, -0.8130406141281128, -0.1913159191608429, -0.05934017896652222]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Tra/Pos', 'RightHand', -0.2618580460548401, 1.021303653717041, -0.13908575475215912, 0.22606851160526276, -0.016798153519630432, 0.4424645006656647, 0.8676614761352539]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Tra/Pos', 'LeftHand', 0.1267719268798828, 1.3903270959854126, -0.07727999985218048, 0.38588353991508484, -0.23801416158676147, -0.777988612651825, 0.43494516611099243]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Tra/Pos', 'Spine', -0.0499625988304615, 0.7079254388809204, 0.0959334447979927, 0.08682862669229507, -0.9752848148345947, 0.0790768414735794, -0.1871558129787445]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Tra/Pos', 'Head', -0.09284941852092743, 1.116592526435852, 0.026464566588401794, 0.05395229905843735, 0.9415268898010254, -0.2129838913679123, 0.255449116230011]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'UpperChest', 8.731149137020111e-09, 0.12410295009613037, -0.013643033802509308, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightLittleDistal', 0.018059253692626953, 0.0006792545318603516, -0.0009430423378944397, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightLittleIntermediate', 0.031398266553878784, -0.00027298927307128906, 2.276897430419922e-05, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightLittleProximal', 0.06326121091842651, 0.003125429153442383, -0.026793677359819412, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightRingDistal', 0.01979649066925049, 0.0007003545761108398, 0.0002487674355506897, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightRingIntermediate', 0.0343228280544281, -0.0005377531051635742, 0.00017258524894714355, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightRingProximal', 0.06668078899383545, 0.008306264877319336, -0.010961335152387619, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightMiddleDistal', 0.022683024406433105, -0.0024051666259765625, 0.0009373053908348083, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightMiddleIntermediate', 0.036900609731674194, -0.001348733901977539, 0.0024379082024097443, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightMiddleProximal', 0.06611180305480957, 0.008372902870178223, 0.004779662936925888, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightIndexDistal', 0.020313501358032227, -0.0006735324859619141, 0.002301432192325592, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightIndexIntermediate', 0.032820671796798706, -0.00013399124145507812, 0.00514911487698555, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightIndexProximal', 0.06390565633773804, 0.005562305450439453, 0.021442245692014694, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightThumbDistal', 0.02131950855255127, -0.001498579978942871, 0.01904042437672615, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightThumbIntermediate', 0.03227710723876953, -0.0026780366897583008, 0.03341251611709595, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightThumbProximal', 0.002032041549682617, -0.006911754608154297, 0.01773388311266899, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftLittleDistal', -0.018059134483337402, 0.0006788969039916992, -0.0009434223175048828, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftLittleIntermediate', -0.03139817714691162, -0.0002732276916503906, 2.2172927856445312e-05, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftLittleProximal', -0.06326128542423248, 0.0031251907348632812, -0.02679355815052986, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftRingDistal', -0.019796311855316162, 0.0007004737854003906, 0.00024871528148651123, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftRingIntermediate', -0.03432297706604004, -0.000537872314453125, 0.0001724734902381897, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftRingProximal', -0.06668068468570709, 0.008306026458740234, -0.01096118614077568, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftMiddleDistal', -0.02268284559249878, -0.0024052858352661133, 0.0009373500943183899, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftMiddleIntermediate', -0.03690075874328613, -0.0013490915298461914, 0.0024379901587963104, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftMiddleProximal', -0.06611169874668121, 0.008372783660888672, 0.0047798119485378265, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftIndexDistal', -0.020313620567321777, -0.0006735324859619141, 0.002301294356584549, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftIndexIntermediate', -0.032820701599121094, -0.0001341104507446289, 0.005148943513631821, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftIndexProximal', -0.06390555202960968, 0.0055620670318603516, 0.021442372351884842, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftThumbDistal', -0.021323874592781067, -0.0015120506286621094, 0.01903466507792473, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftThumbIntermediate', -0.0322871208190918, -0.0027006864547729492, 0.03340105712413788, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftThumbProximal', -0.0020318031311035156, -0.006911873817443848, 0.017733890563249588, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightEye', 0.02083751931786537, 0.06215071678161621, 0.028359852731227875, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftEye', -0.020837578922510147, 0.06215071678161621, 0.02835983783006668, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightToes', 0.001465722918510437, -0.0633649230003357, 0.11059761047363281, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftToes', -0.0014662444591522217, -0.06336498260498047, 0.110597625374794, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightHand', 0.21357405185699463, -0.00046515464782714844, 0.01762925460934639, 0.03633392974734306, -0.3689839243888855, -0.04793441295623779, 0.9274874925613403]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftHand', -0.2135740965604782, -0.00046563148498535156, 0.017629820853471756, -0.6872519254684448, -0.14427059888839722, -0.4700693190097809, 0.5347014665603638]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightLowerArm', 0.21900662779808044, -0.01019740104675293, 0.001837126910686493, 0.1812271773815155, 0.8595459461212158, -0.25001946091651917, -0.40721964836120605]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftLowerArm', -0.21900661289691925, -0.010197639465332031, 0.0018373653292655945, -0.3288290202617645, 0.2270265370607376, -0.034346334636211395, 0.9160518050193787]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightUpperArm', 0.08629468828439713, -0.014869213104248047, 0.005363732576370239, -0.10367128998041153, -0.0719507560133934, -0.2778274416923523, 0.95230633020401]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftUpperArm', -0.08629469573497772, -0.014869213104248047, 0.005363747477531433, -0.01071480754762888, 0.11505597084760666, -0.5041602253913879, 0.8558445572853088]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightShoulder', 0.022385641932487488, 0.08726394176483154, -0.027442801743745804, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftShoulder', -0.022385630756616592, 0.08726394176483154, -0.027442824095487595, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'Head', -2.500019036233425e-08, 0.07220947742462158, 0.009306959807872772, -0.005485386122018099, 0.08571496605873108, 0.008622481487691402, -0.9962673187255859]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'Neck', 8.061761036515236e-09, 0.11461520195007324, -0.03339564427733421, 0.14929604530334473, 0.031815055757761, 0.13663029670715332, 0.9787904620170593]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'Chest', -1.1961674317717552e-08, 0.1139877438545227, 0.014169316738843918, -0.0, -0.0, -0.0, 1.0]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'Spine', -8.556526154279709e-09, 0.052913784980773926, 0.00982462614774704, -0.08879011869430542, 0.00019472837448120117, 0.10003677755594254, -0.9910141229629517]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightFoot', -0.010351940989494324, -0.4146432876586914, -0.021174781024456024, -0.036094751209020615, 0.19802197813987732, -0.008350386284291744, 0.9794972538948059]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftFoot', 0.010351166129112244, -0.4146432876586914, -0.021174591034650803, -0.20442330837249756, 0.20251162350177765, 0.07975900918245316, 0.9543787837028503]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightLowerLeg', -0.020266607403755188, -0.35218948125839233, -0.006325429771095514, -0.5436118841171265, 0.02874111197888851, -0.02317030541598797, 0.8385244607925415]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftLowerLeg', 0.020266175270080566, -0.35218995809555054, -0.0063255345448851585, 0.2926897704601288, -0.0211596991866827, 0.36723968386650085, 0.8826211094856262]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'RightUpperLeg', 0.07712236791849136, -0.03995406627655029, -0.003501297440379858, -0.1434360295534134, 0.010406479239463806, 0.038393285125494, -0.9888598322868347]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'LeftUpperLeg', -0.07712239027023315, -0.03995388746261597, -0.0035012857988476753, 0.07695823162794113, 0.025184735655784607, -0.12362191081047058, -0.9890201091766357]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Bone/Pos', 'Hips', 0.00029858737252652645, 0.9276769757270813, 0.005808895919471979, -0.005086240358650684, 0.9822646975517273, 0.026934944093227386, 0.18548519909381866]
+... but no matching route
+
+OSC - Receiving:['/VMC/Ext/Root/Pos', 'root', 0.05278107151389122, -0.27312424778938293, -0.0965588241815567, 0.0, 180.0, 0.0, 1.0]
+... but no matching route
+
